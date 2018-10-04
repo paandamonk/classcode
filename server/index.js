7b831cd5366af06cb481f508517f1d7248f00f13
Robert Williams <paandamonk@gmail.com>
2018-10-04 12:17:55 -0400

    New class stuff 10/4/18

--- server/index.js
+++ server/index.js
@@ -1,6 +1,17 @@
 const math = require('./math');
+const express = require('express');
 
-
-var something = math.add(2, 2);
 
 console.log("2 + 2 = " + something);
+
+const app = express();
+
+app.get("/add", function(req, res){
+    var something = math.add(2, 2);
+    res.send(something);
+});
+app.use("/",function(req, res, next){
+    res.send( "hello world");
+});
+
+app.listen(3000);
